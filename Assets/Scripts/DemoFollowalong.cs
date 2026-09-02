a58fb0602fde8ff02ce9c377783336792971dadc
Lisa Chow <mochiimurasaki@gmail.com>
2026-09-02 14:52:43 -0400

    demo in new scene

---
+++ Assets/Scripts/DemoFollowalong.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class DemoFollowalong : MonoBehaviour
+{
+  //DEMO IN CLASS SCRIPT EXAMPLES 
+  // public float floatVar = 3.5f;
+  // public int intVar = 10;
+  // public string stringVar = "Hello world";
+
+
+  // public List<float> listDemo;
+  //  public string[] arrayDemo;
+
+    //public GameObject objDemo;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created <-- it is done once and never happens again
+    void Start()
+    {
+        print("hello");
+
+    }
+
+    // Update is called once per frame <-- keeps reading the code
+    void Update()
+    {
+        print("hello");
+    }
+}
