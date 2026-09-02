ee74a832e8208a6c662f34a8cf0bdbd59fc6f134
Aiyayai.exe <Ysanne007@gmail.com>
2026-09-02 14:27:34 -0400

    in class

---
+++ Assets/Scenes/inclass.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class gjhgkh : MonoBehaviour
+{
+    public float floatVar = 3.5f;
+    public int intVar = 10;
+    public string stringVar = "hello world";
+
+    public list<float> listVar;
+    public string[] arrayDemo;
+
+    public Gameobject cube (4);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("konnichiwassup");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
