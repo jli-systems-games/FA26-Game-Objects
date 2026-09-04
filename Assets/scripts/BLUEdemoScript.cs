f64cc546ff7a6462ec588ba12c9ca92bc0f10d74
Lisa Chow <mochiimurasaki@gmail.com>
2026-09-04 13:55:49 -0400

    demo week2

---
+++ Assets/scripts/BLUEdemoScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BLUEdemoScript : MonoBehaviour
+{
+    public GameObject blueCube;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+     //transform.position = Vector3.Lerp(transform.position, blueCube.transform.position, Time.deltaTime);
+     //^following target cube (blue)
+     //transform.position += transform.forward * Time.deltaTime;
+     //^moving forward in the direction the cube is facing
+
+
+    }
+}
