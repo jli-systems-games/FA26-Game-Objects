2f7f01fded688a1537594c0640bece01863df0a8
Lisa Chow <mochiimurasaki@gmail.com>
2026-09-08 04:02:15 -0400

    change color to pink, cannot change back to purple

--- Assets/scripts/CUBE.cs
+++ Assets/scripts/CUBE.cs
@@ -6,10 +6,17 @@ public class CUBE : MonoBehaviour
     public Vector2 moveInput;
     public InputActionReference moveAction;
 
+    public MeshRenderer renderer;
+
+    public Material myMaterial1; 
+    public Material myMaterial2;
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        renderer = GetComponent<MeshRenderer>(); 
+
     }
 
     // Update is called once per frame
@@ -18,5 +25,24 @@ public class CUBE : MonoBehaviour
         moveInput = moveAction.action.ReadValue<Vector2>(); 
         transform.position += new Vector3(moveInput.x, moveInput.y, 0) * moveSpeed * Time.deltaTime; 
 
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            Debug.Log("E key was pressed"); 
+           // ChangeColor();
+           renderer.material = myMaterial2;
+        }
+
     }
+    public void ChangeColor() 
+    {
+        if (renderer.material == myMaterial1)
+        {
+            renderer.material = myMaterial2;
+        }
+        else
+        {
+            renderer.material = myMaterial1;
+        }
+    }
+    
 }
