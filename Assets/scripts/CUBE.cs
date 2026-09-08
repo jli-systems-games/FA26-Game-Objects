e7080bd4d4d9cad1f849e78694a7dbf62b27e87b
Lisa Chow <mochiimurasaki@gmail.com>
2026-09-08 02:42:45 -0400

    movement

--- Assets/scripts/CUBE.cs
+++ Assets/scripts/CUBE.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class CUBE : MonoBehaviour
 {
+    public float moveSpeed; 
+    public Vector2 moveInput;
+    public InputActionReference moveAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,8 @@ public class CUBE : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput = moveAction.action.ReadValue<Vector2>(); 
+        transform.position += new Vector3(moveInput.x, moveInput.y, 0) * moveSpeed * Time.deltaTime; 
+
     }
 }
