eb787885b38ba481dcf4245f20f796892e0bea34
Felicity <zhanf463@newschool.edu>
2026-09-09 00:14:17 -0400

    Week 2

--- Assets/Move Script.cs
+++ Assets/Move Script.cs
@@ -9,7 +9,7 @@ public class MoveScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+     MoveAction.action.Enable();
     }
 
     // Update is called once per frame
