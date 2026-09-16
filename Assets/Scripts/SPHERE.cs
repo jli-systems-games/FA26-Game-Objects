6924066b32a60f4c6022ec5047f8d5bacb169274
Lisa Chow <mochiimurasaki@gmail.com>
2026-09-16 00:28:47 -0400

    CAMERA 1 SET UP

--- Assets/Scripts/SPHERE.cs
+++ Assets/Scripts/SPHERE.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class DeleteLater : MonoBehaviour
+public class SPHERE : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
