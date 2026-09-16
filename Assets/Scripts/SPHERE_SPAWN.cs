8a4b8eca5700822a9cb3a6ae45984071bb265ac8
Lisa Chow <mochiimurasaki@gmail.com>
2026-09-16 01:56:24 -0400

    sphere spawn movement (script)

---
+++ Assets/Scripts/SPHERE_SPAWN.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SPHERE_SPAWN : MonoBehaviour
+{
+
+    public float speed = 2.0f;
+    public float distance = 3.0f;
+    public Vector3 startPosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newX = startPosition.x + Mathf.Sin(Time.time * speed) * distance;
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+}
