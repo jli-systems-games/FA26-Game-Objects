613bffa93341e84107841bc7a90752dc814c9263
Lisa Chow <mochiimurasaki@gmail.com>
2026-09-16 15:19:22 -0400

    no message

--- Assets/Scripts/RandomText.cs
+++ Assets/Scripts/RandomText.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI; 
+
 using TMPro;
 public class RandomText : MonoBehaviour
 {
@@ -9,7 +11,7 @@ public class RandomText : MonoBehaviour
     public List<int> randomNumbers;
 
     [TextArea(3, 10)] // Optional: Set the size of the text area in the Inspector
-    
+
 
     public TMP_Text phrasesObj; // Reference to the TMP_Text component
 
@@ -18,12 +20,14 @@ public class RandomText : MonoBehaviour
 
     void Update()
     {
-        //phrasesObj.text = randomPhrases[6]; <-- display 6
-        phrasesObj.text = randomPhrases[Random.Range(0, randomPhrases.Count)]; // Display a random phrase from the list
+
     }
 
     public void RandomizeText()
     {
         phrasesObj.text = "this number is: " + randomPhrases[0];
+        //phrasesObj.text = randomPhrases[6]; <-- display 6
+        phrasesObj.text = randomPhrases[Random.Range(0, randomPhrases.Count)]; // Display a random phrase from the list
     }
+
 }
