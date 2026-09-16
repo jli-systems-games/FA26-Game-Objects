6339895c12c2f0ee4b52eed9d596ddb33e89edb8
Lisa Chow <mochiimurasaki@gmail.com>
2026-09-16 02:33:07 -0400

    no message

--- Assets/Scripts/SPHERE_SPAWN.cs
+++ Assets/Scripts/SPHERE_SPAWN.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 public class SPHERE_SPAWN : MonoBehaviour
 {
 
+    public Transform sphereSpawn;
+    public GameObject spherePrefab;
+    
     public float speed = 2.0f;
     public float distance = 3.0f;
     public Vector3 startPosition;
+    public float timer = 0f; 
+    public float checkInterval = 2.0f; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,5 +23,12 @@ public class SPHERE_SPAWN : MonoBehaviour
     {
         float newX = startPosition.x + Mathf.Sin(Time.time * speed) * distance;
         transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+
+        timer += Time.deltaTime;
+        if (timer >= checkInterval)
+        {
+            Instantiate(spherePrefab, sphereSpawn.position, sphereSpawn.rotation);
+            timer -= checkInterval;
+        }
     }
 }
