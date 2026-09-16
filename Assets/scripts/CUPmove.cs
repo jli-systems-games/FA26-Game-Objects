4a6f57be60922241fb22e731a8aebeb79f4d2b96
Lisa Chow <mochiimurasaki@gmail.com>
2026-09-16 02:36:32 -0400

    no message

---
+++ Assets/scripts/CUPmove.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CUPmove : MonoBehaviour
+
+
+
+{
+
+    public float moveSpeed;
+    private Vector2 moveInput; 
+    public InputActionReference moveAction; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = moveAction.action.ReadValue<Vector2>(); //reads the input values from the input action reference and stores them in the moveInput variable
+        transform.position += new Vector3(moveInput.x, 0, 0) * moveSpeed * Time.deltaTime; 
+    }
+}
+ 
+ 
