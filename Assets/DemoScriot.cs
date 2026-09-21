65bc77833098fce3774065845751561a72b179ab
Karina Umekubo <>
2026-09-21 12:21:13 -0400

    week 4 demo example push

---
+++ Assets/DemoScriot.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+
+public class LoopExample: MonoBehaviour
+{
+    public GameObject[] greenCubes;
+    public float timer = 30f;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        greenCubes = GameObject.FindObjectsWithTag("green");
+
+        for (int i= 0; i >= timer; i++)
+        {
+            //code inside here happens
+            print ("I variable is now" + i.ToString ());
+
+        }
+        print("You finished the loop");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            forLoopContainer();
+        }
+    }
+    public void forLoopContainer() 
+    {
+        for (int i = 0 ; i <= timer; i++)
+        {
+            print("I variable is now" + i.ToString());
+        }
+    }
+
+    public IEnumerator()
+    {
+        timer -= 1;
+        yield return new WaitForSeconds(3f);
+        print("Timer is now: " + timer);
+    }
+}
