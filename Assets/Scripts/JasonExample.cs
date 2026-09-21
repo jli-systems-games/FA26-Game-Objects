e91c11b4355d39e005353bfb0837a8ea854bbf3a
Karina Umekubo <>
2026-09-21 12:34:19 -0400

    Example commit from Jason

---
+++ Assets/Scripts/JasonExample.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class JasonExample : MonoBehaviour
+{
+    public float number;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log($"Number is {number}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
