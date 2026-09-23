9dabf51e7ce5c0f30e535dd0f219ac46a3b42090
Felicity <zhanf463@newschool.edu>
2026-09-23 00:52:20 -0400

    Week4

---
+++ Assets/randomize words.cs
@@ -0,0 +1,16 @@
+using TMPro;
+using UnityEngine;
+
+public class randomizewords : MonoBehaviour
+{
+    public TMP_Text words;
+    public string[] AltTitles = new string[6];
+    public string newTitle;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void WordsChange()
+    {
+        newTitle = AltTitles[Random.Range(0, AltTitles.Length)];
+        words.text = newTitle;
+    }
+}
